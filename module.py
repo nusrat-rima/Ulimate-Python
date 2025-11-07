7b4873b71ded210229767b6e1900ea57950579e6
nusrat-rima <nusratrima3151@gmail.com>
2025-11-07 22:14:35 +0600

    added first module in python

---
+++ module.py
@@ -0,0 +1,11 @@
+print("python module")
+import pyjokes
+
+joke = pyjokes.get_joke()
+# printing jokes -single line command
+"""
+multiple line command
+so thanks 
+that was my program
+"""
+print(joke)
