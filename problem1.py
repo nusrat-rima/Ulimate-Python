bf9d26b7d16e4f5c9fd3882a8af7eb3fa55b1e7d
nusrat-rima <nusratrima3151@gmail.com>
2025-11-07 22:23:02 +0600

    first problem solve using python

---
+++ problem1.py
@@ -0,0 +1,15 @@
+print('''Twinkle twinkle little star.
+How I wonder what you are.
+Up above the world so high.
+Like a diamond in the sky.
+Twinkle twinkle little star.
+How I wonder what you are.
+
+Twinkle twinkle little star.
+How I wonder what you are.
+Up above the world so high.
+Like a diamond in the sky.
+Twinkle twinkle little star.
+How I wonder what you are.''')
+
+# by using ''' we can use multilines code print 
