4b8e96df6e9aa7ba59793acf7f975ab981c42384
nusrat-rima <nusratrima3151@gmail.com>
2026-07-30 19:35:38 +0600

    added input function

---
+++ _input.py
@@ -0,0 +1,16 @@
+a = input("Enter number 1 : ")
+
+b = input("Enter number 2 : ")
+
+print ("Number a is :", a)
+print ("Number b is :", b)
+print ("Sum is ", a+b) # this will concat the numbers that it get from the user because type function take inputs as string fromats and then concat it.
+
+# this is the correct version of it
+a = int(input("Enter number 1 : "))
+
+b = int(input("Enter number 2 : "))
+
+print("Number a is :", a)
+print("Number b is :", b)
+print("Sum is ", a + b)
