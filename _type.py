00c6686190b3f45ed0c070341b76ea5aa53007af
nusrat-rima <nusratrima3151@gmail.com>
2026-07-30 19:27:34 +0600

    type function in python

---
+++ _type.py
@@ -0,0 +1,8 @@
+a = 23
+b = type(a)
+print(b) # returns <class 'int'>
+
+c = "31.3"  # here c represents string but it should be float
+d =  float(c) 
+t = type(d)
+print(t)  #  returns <class 'float'>
