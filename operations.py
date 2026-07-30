3fb58a83247c14a41dc58184ec22b9570471c72f
nusrat-rima <nusratrima3151@gmail.com>
2026-07-30 19:07:25 +0600

    added different types of operations in pyhton

---
+++ operations.py
@@ -0,0 +1,29 @@
+#Arithmetic operators
+a=7
+b=10
+c = a/b
+print (c)
+
+#Assignment Operators
+a= 4-2 #assign 4-2 in a
+print (a)
+b = 7
+b += 3 #Increment the value of b and then assign it to b
+b -= 3 # Decrement the value of b and then assign it to b
+print(b)
+
+
+#Comparison operators
+ 
+d = 5<4
+print (d) #returns false
+
+e = 5!=5
+print (e) #returns false
+
+
+#logical operators
+
+f = True or False
+print (f) #returns true 
+print (not (False)) #returns true
